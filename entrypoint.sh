1184075b7190661723fde852818417f63a457dc9
saket28 <saket28@hotmail.com>
2023-03-06 09:34:07 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ echo importing data...
 # run the init script to create the DB and the tables in /table
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./init.sql
 
-for entry in "table/*.sql"
+for entry in "data/*.sql"
 do
   echo executing $entry
   /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i $entry
